66ce88862639c89be48a939dcbc75b2c3c0ecb5b
Paul Bonser <pib@paulbonser.com>
2009-02-11 23:14:56 -0600

    add makefile, orgainze so it's easy to include in a mochiweb deps dir

--- src/openid.erl
+++ src/openid.erl
@@ -1,6 +1,6 @@
 %%%-------------------------------------------------------------------
-%%% File : openid.erl
-%%% Author : Paul Bonser <pib@paulbonser.com>
+%%% File    : openid.erl
+%%% Author  : Paul Bonser <pib@paulbonser.com>
 %%% Description : Implement the OpenId 1.1 "dumb mode" consumer protocol
 %%%
 %%% Created : 20 Jan 2009 by Paul Bonser <pib@paulbonser.com>
@@ -11,12 +11,12 @@
 -export([discover/1, checkid_setup_url/2, checkid_setup_url/3, check_authentication/2]).
 
 start_authentication(Identifier, ReturnTo) ->
-	DiscoResponse = discover(Identifier),
-	checkid_setup_url(DiscoResponse, ReturnTo).
+    DiscoResponse = discover(Identifier),
+    checkid_setup_url(DiscoResponse, ReturnTo).
 
 finish_authentication(Params) ->
-	Server = proplists:get_value("identity_provider", Params),
-	case proplists:get_value("openid.mode", Params) of
+    Server = proplists:get_value("identity_provider", Params),
+    case proplists:get_value("openid.mode", Params) of
         "id_res" ->
             case check_authentication(Server, Params) of
                 true ->
@@ -31,9 +31,9 @@ finish_authentication(Params) ->
     end.
 
 check_authentication(Server, Params) ->
-	Params2 = proplists:delete("openid.mode", Params),
-
-	case http:request(post, {
+    Params2 = proplists:delete("openid.mode", Params),
+    
+    case http:request(post, {
                         Server,
                         [],
                         "application/x-www-form-urlencoded",
@@ -48,50 +48,50 @@ check_authentication(Server, Params) ->
                 _ -> false
             end;
         Error -> Error
-	end.
+    end.
 
 parse_key_value(KeyValueString) ->
-	lists:map(fun(Line) -> list_to_tuple(string:tokens(Line, ":")) end,
+    lists:map(fun(Line) -> list_to_tuple(string:tokens(Line, ":")) end, 
               string:tokens(KeyValueString, "\n")).
 
 checkid_setup_url(Params, ReturnTo) ->
-	checkid_setup_url(Params, ReturnTo, ReturnTo).
+    checkid_setup_url(Params, ReturnTo, ReturnTo).
 
 checkid_setup_url(Params, ReturnTo, TrustRoot) ->
-	Server = proplists:get_value(server, Params),
-	Identifier = proplists:get_value(identifier, Params),
-	Delegate = proplists:get_value(delegate, Params, Identifier),
-
-	Server ++ "?" ++ mochiweb_util:urlencode(
-                       [
-                        {"openid.mode", "checkid_setup"},
-                        {"openid.identity", Delegate},
-                        {"openid.return_to", ReturnTo ++ "?" ++
-                         mochiweb_util:urlencode(
-                           [
-                            {"original_identifier", Identifier},
-                            {"identity_provider", Server}
-                           ])},
-                        {"openid.trust_root", TrustRoot}
-                       ]).
+    Server = proplists:get_value(server, Params),
+    Identifier = proplists:get_value(identifier, Params),
+    Delegate = proplists:get_value(delegate, Params, Identifier), 
+    
+    Server ++ "?" ++ mochiweb_util:urlencode(
+                [
+                 {"openid.mode", "checkid_setup"},
+                 {"openid.identity", Delegate},
+                 {"openid.return_to", ReturnTo ++ "?" ++ 
+                  mochiweb_util:urlencode(
+                    [
+                     {"original_identifier", Identifier},
+                     {"identity_provider", Server}
+                    ])},
+                 {"openid.trust_root", TrustRoot}
+                ]).
 
 discover(Identifier) ->
-	NormalizedIdentifier = normalize_identifier(Identifier),
-	case http:request(NormalizedIdentifier) of
+    NormalizedIdentifier = normalize_identifier(Identifier),
+    case http:request(NormalizedIdentifier) of
         {ok, {_Status, _Headers, Body}} ->
             HtmlTokens = mochiweb_html:parse(Body),
             [{identifier, NormalizedIdentifier} | find_openid_tags(HtmlTokens)];
         _ ->
             {error, http_error}
-	end.
+    end.
 
 normalize_identifier(Ident = "http://" ++ _Rest) ->
-	Ident;
+    Ident;
 normalize_identifier(Ident) ->
-	"http://" ++ Ident.
+    "http://" ++ Ident.
 
 find_openid_tags(HtmlTokens) ->
-	case find_tag(<<"head">>, [HtmlTokens]) of
+    case find_tag(<<"head">>, [HtmlTokens]) of
         {<<"head">>, _Attrs, Children, _Rest} ->
             case find_tag_with_attr(<<"link">>, {<<"rel">>, <<"openid.server">>}, Children) of
                 not_found ->
@@ -109,21 +109,21 @@ find_openid_tags(HtmlTokens) ->
             end;
         not_found ->
             {error, no_head_tag}
- 	end.
+    end.
 
 find_tag(_TagName, []) ->
- 	not_found;
+    not_found;
 find_tag(TagName, [{TagName, Attributes, Children} | Rest]) ->
- 	{TagName, Attributes, Children, Rest};
+    {TagName, Attributes, Children, Rest};
 find_tag(TagName, [{_OtherTag, _Attributes, Children} | Rest]) ->
- 	find_tag(TagName, Children ++ Rest);
+    find_tag(TagName, Children ++ Rest);
 find_tag(TagName, [_Other | Rest]) ->
- 	find_tag(TagName, Rest).
+    find_tag(TagName, Rest).
 
 find_tag_with_attr(_TagName, {_AttrKey, _AttrVal}, []) ->
- 	not_found;
+    not_found;
 find_tag_with_attr(TagName, Attr = {AttrKey, AttrVal}, Tags) ->
- 	case find_tag(TagName, Tags) of
+    case find_tag(TagName, Tags) of
         not_found ->
             not_found;
         {TagName, Attributes, Children, Rest} ->
@@ -133,4 +133,4 @@ find_tag_with_attr(TagName, Attr = {AttrKey, AttrVal}, Tags) ->
                 _ ->
                     find_tag_with_attr(TagName, Attr, Children ++ Rest)
             end
- 	end. 
+    end.
